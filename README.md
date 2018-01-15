b1f5b8ecc7d4eb71a18ede91c6be6b0bd715339c
Garret Okamoto <gokamoto@gmail.com>
2018-01-15 12:03:58 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask Sample Application
+
+This repository provides a sample Python web application implemented using the Flask web framework and hosted using ``gunicorn``. It is intended to be used to demonstrate deployment of Python web applications to OpenShift 3.x
+
+I am currently printing some information on where the app is being served to see what will be output when I put this on OpenShift.
+
+The pig latin output was inspired by my 7th-grade daughter doing her pig latin app for Code Academy this weekend. I think I took a shortcut and made my code compact, but maybe I'll use her code later to make her happy. :)
---
+++ wsgi.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import socket
+from datetime import date
+
+application = Flask(__name__)
+
+
+def pig_latin(word):
+    if word[0] in "aeiou":
+        return word + 'yay'
+    return word[1:] + word[0] + 'ay'
+
+
+@application.route("/")
+def countdown():
+    today = date.today()
+    april6 = date(2018, 4, 1)
+    str_to_show = "C2C24 Demo Python App \r\n Number of days to April 6:"
+
+    original_word = input('Enter something to be converted into pig latin: ')
+    piglatin_word = pig_latin(original_word)
+
+    if april6 > today:
+        time_to_april6 = abs(april6 - today)
+        days_to_april6 = time_to_april6.days
+        str_to_show += str(days_to_april6)
+    return "This is a test served from {} to {}. {}. Input was {}".format(socket.gethostname(), request.remote_addr, str_to_show, piglatin_word)
+
+
+if __name__ == "__main__":
+    # application.run(host='0.0.0.0')
+    application.run()
