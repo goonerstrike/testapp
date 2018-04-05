47d4b4d0d7964f99088702bf90e4b4a6a9d69e85
gokamoto-publicrepo <34248015+gokamoto-publicrepo@users.noreply.github.com>
2018-04-05 10:53:18 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-#import socket
 from datetime import date
 
 application = Flask(__name__)
@@ -15,19 +14,18 @@ def pig_latin(word):
 @application.route("/")
 def countdown():
     today = date.today()
-    april6 = date(2018, 4, 1)
-    str_to_show = "Demo Python App \r\n Number of days to April 6:"
+    therandomdate = date(2018, 6, 19)
+    str_to_show = "Demo Python App.    Number of days to June 19:"
     
     #original_word = input('Enter something to be converted into pig latin: ')
     original_word = "Faith"
     piglatin_word = pig_latin(original_word)
 
-    if april6 > today:
-        time_to_april6 = abs(april6 - today)
-        days_to_april6 = time_to_april6.days
-        str_to_show += str(days_to_april6)
-    # return "This is a test served from {} to {}. {}. Pig latin version of Faith is {}".format(socket.gethostname(), request.remote_addr, str_to_show, piglatin_word)
-    return "{}. Pig latin version of Faith is {}".format(str_to_show, piglatin_word)
+    if therandomdate > today:
+        time_to_therandomdate = abs(therandomdate - today)
+        days_to_therandomdate = time_to_therandomdate.days
+        str_to_show += str(days_to_therandomdate)
+    return "{}.\r\n Pig latin version of Faith is {}".format(str_to_show, piglatin_word)
 
 if __name__ == "__main__":
     # application.run(host='0.0.0.0')
